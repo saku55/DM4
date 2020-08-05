4f614b54c2085057e3a6781275417e0b7c489dea
saku55 <69268653+saku55@users.noreply.github.com>
2020-08-06 04:51:14 +0900

    Hot: Initial revision

---
+++ ex4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i,num,heads=0,tails=0;
+
+    for(i=0; i<3; i++){
+        num=rand();
+        if(num%2==0){
+            printf("Round %d: Head\n", i+1);
+            heads++;
+        } else{
+            printf("Round %d: Tail\n", i+1);
+            tails++;
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    return 0;
+}
